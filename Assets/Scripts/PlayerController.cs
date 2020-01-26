7115fbe10703cffb93a6ce50234835390469971e
Jordan Orelli <jordanorelli@gmail.com>
2020-01-26 09:45:01 -0600

    hello project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float moveSpeed;
+    public float jumpForce;
+    public CharacterController cc;
+    public float gravityScale;
+
+    private Vector3 moveDirection;
+
+    void Start() {
+        cc = GetComponent<CharacterController>();
+    }
+
+    void Update() {
+        float dx = Input.GetAxis("Horizontal") * moveSpeed;
+        moveDirection = new Vector3(dx, moveDirection.y, 0f);
+        if (cc.isGrounded) {
+            if (Input.GetButtonDown("Jump")) {
+                moveDirection.y = jumpForce;
+            } else {
+                moveDirection.y = moveDirection.y + Physics.gravity.y*gravityScale*Time.deltaTime;
+                if (moveDirection.y < 0f) {
+                    moveDirection.y = 0f;
+                }
+            }
+        } else {
+            moveDirection.y = moveDirection.y + Physics.gravity.y*gravityScale*Time.deltaTime;
+        }
+        Debug.Log(moveDirection);
+        cc.Move(moveDirection * Time.deltaTime);
+    }
+
+    void FixedUpdate() {
+        float motor = Input.GetAxis("Vertical");
+        float steering = Input.GetAxis("Horizontal");
+    }
+}

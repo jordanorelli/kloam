e1caedcccba480cbd74c34d7c88cf5696d3dd4ba
Jordan Orelli <jordanorelli@gmail.com>
2020-02-02 11:53:50 -0600

    working on a login screen

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Networking networking;
+    public InputField usernameField;
+    public InputField passwordField;
+
+    // Start is called before the first frame update
+    void Start() {
+        networking.Connect();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    public void Login() {
+        string username = usernameField.text.Trim();
+        string password = passwordField.text.Trim();
+        if (username == "" || password == "") {
+            return;
+        }
+        usernameField.interactable = false;
+        passwordField.interactable = false;
+        networking.SendLogin(username, password);
+    }
+}
--- Assets/Scripts/Networking.cs
+++ Assets/Scripts/Networking.cs
@@ -19,10 +19,13 @@ public class Networking : ScriptableObject {
     private ClientWebSocket sock;
     private Task connectTask;
     private Task writeTask;
-    private bool isConnected;
     private int seq;
 
     public void Connect() {
+        if (isConnected()) {
+            return;
+        }
+        Application.quitting += autoDisconnect;
         sock = new ClientWebSocket();
         UriBuilder b = new UriBuilder();
         b.Scheme = "ws";
@@ -33,7 +36,6 @@ public class Networking : ScriptableObject {
         connectTask = sock.ConnectAsync(b.Uri, CancellationToken.None);
         connectTask.ContinueWith((x) => {
             Debug.LogFormat("Finished connection task with status: {0}", sock.State);
-            isConnected = sock.State == WebSocketState.Open;
         });
     }
 
@@ -60,6 +62,29 @@ public class Networking : ScriptableObject {
         sock.SendAsync(buf, WebSocketMessageType.Text, true, CancellationToken.None);
     }
 
+    public void SendLogin(string username, string password) {
+        seq++;
+        Login login;
+        login.seq = seq;
+        login.cmd = "login";
+        login.username = username;
+        login.password = password;
+        string msg = JsonUtility.ToJson(login);
+        ArraySegment<byte> buf = new ArraySegment<byte>(Encoding.UTF8.GetBytes(msg));
+        sock.SendAsync(buf, WebSocketMessageType.Text, true, CancellationToken.None);
+    }
+
+    private void autoDisconnect() {
+        if (isConnected()) {
+            Task task = sock.CloseAsync(WebSocketCloseStatus.NormalClosure, "closed", CancellationToken.None);
+            task.Wait();
+        }
+    }
+
+    private bool isConnected() {
+        return sock != null && sock.State == WebSocketState.Open;
+    }
+
     private struct CollectSoul {
         public int seq;
         public string cmd;
@@ -71,5 +96,12 @@ public class Networking : ScriptableObject {
         public int seq;
         public string cmd;
         public Vector3 position;
+    }
+
+    private struct Login {
+        public int seq;
+        public string cmd;
+        public string username;
+        public string password;
     }
 }
